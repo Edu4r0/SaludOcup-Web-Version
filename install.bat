505fb260cb2524455a7cdd026333f9ae8f28d33a
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-04-14 09:14:40 -0500

    pip install

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-pip install PyQtWebEngine
-pip install PyQt5
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pip install PyQtWebEngine
+pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pip install PyQt5
 python --version
 PAUSE
