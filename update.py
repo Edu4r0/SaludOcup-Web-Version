7cac359a3dd6c880fafae0b16cf42b134a4f1797
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-04-17 11:29:33 -0500

    OK

--- update.py
+++ update.py
@@ -1,27 +1,27 @@
-from urllib.request import urlretrieve
+import urllib.request 
 
 urls = [
-    "https://github.com/Edu4r0/SaludOcup-Web-Version/blob/4bcd7a65bbd8a1a045e238b774ee6259bde0719c/url.py",
-    "https://github.com/Edu4r0/SaludOcup-Web-Version/blob/4bcd7a65bbd8a1a045e238b774ee6259bde0719c/imports.py",
-    "https://github.com/Edu4r0/SaludOcup-Web-Version/blob/4bcd7a65bbd8a1a045e238b774ee6259bde0719c/js/index.js",
-    "https://github.com/Edu4r0/SaludOcup-Web-Version/blob/4bcd7a65bbd8a1a045e238b774ee6259bde0719c/html/index.html",
-    "https://github.com/Edu4r0/SaludOcup-Web-Version/blob/4bcd7a65bbd8a1a045e238b774ee6259bde0719c/html/poppu.html",
-    "https://github.com/Edu4r0/SaludOcup-Web-Version/blob/4bcd7a65bbd8a1a045e238b774ee6259bde0719c/css/button.css",
-    "https://github.com/Edu4r0/SaludOcup-Web-Version/blob/4bcd7a65bbd8a1a045e238b774ee6259bde0719c/css/poppu.css",
-    "https://github.com/Edu4r0/SaludOcup-Web-Version/blob/4bcd7a65bbd8a1a045e238b774ee6259bde0719c/css/style.css"
+    "https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/url.py",
+    "https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/imports.py",
+    "https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/js/index.js",
+    "https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/html/index.html",
+    "https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/html/poppu.html",
+    "https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/css/button.css",
+    "https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/css/poppu.css",
+    "https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/css/style.css"
 ]
 
 file_paths = [
-    "C:\Temp\SaludOcup-Web-Version\url.py",
-    "C:\Temp\SaludOcup-Web-Version\imports.py",
-    "C:\Temp\SaludOcup-Web-Version\js\index.js",
-    "C:\Temp\SaludOcup-Web-Version\html\index.html",
-    "C:\Temp\SaludOcup-Web-Version\html\poppu.html",
-    "C:\Temp\SaludOcup-Web-Version\css\button.css",
-    "C:\Temp\SaludOcup-Web-Version\css\poppu.css",
-    "C:\Temp\SaludOcup-Web-Version\css\style.csss"
+    "C:/Temp/SaludOcup-Web-Version/url.py",
+    "C:/Temp/SaludOcup-Web-Version/imports.py",
+    "C:/Temp/SaludOcup-Web-Version/js/index.js",
+    "C:/Temp/SaludOcup-Web-Version/html/index.html",
+    "C:/Temp/SaludOcup-Web-Version/html/poppu.html",
+    "C:/Temp/SaludOcup-Web-Version/css/button.css",
+    "C:/Temp/SaludOcup-Web-Version/css/poppu.css",
+    "C:/Temp/SaludOcup-Web-Version/css/style.csss"
 ]
 
 def actualizar():
     for i, url in enumerate(urls):
-        urlretrieve(url, file_paths[i])
+        urllib.request.urlretrieve(url, file_paths[i])
