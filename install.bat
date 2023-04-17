50fe3dc00f3aa3f18a2fed0f9076d2a1370c6ffb
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-04-17 16:03:32 -0500

    OK

--- install.bat
+++ install.bat
@@ -2,4 +2,10 @@ pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pip in
 pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pip install PyQt5
 pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org pip install GitPython
 python --version
-PAUSE
+@echo off
+echo Descargando Git...
+curl -LOk https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe
+echo Instalando Git...
+start /wait Git-2.31.1-64-bit.exe /SILENT
+echo Git instalado exitosamente.
+pause
