dcea077b18d2e8757c08b5081865e380bf7e77c7
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-04-17 17:26:44 -0500

    Ok

---
+++ update.py
@@ -0,0 +1,13 @@
+import git
+
+# Clone the repository to a new directory named 'SaludOcup-Web-Version'
+try:
+    git.Repo.clone_from('https://github.com/Edu4r0/SaludOcup-Web-Version.git', 'C:Temp\SaludOcup-Web-Version')
+except git.exc.GitCommandError:
+    pass
+
+# Open the existing repository in the 'SaludOcup-Web-Version' directory
+repo = git.Repo('C:\Temp\SaludOcup-Web-Version')
+
+# Pull the latest changes from the remote repository
+repo.remotes.origin.pull()
