9ecbdc00c07691eccbce45a22e8559f3047139f2
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-04-19 15:30:52 -0500

    OK

--- others/update.py
+++ others/update.py
@@ -1,12 +1,13 @@
-import wget
 import subprocess
+import urllib.request
 
-wget.download('https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/others/install','C:\Temp\install.bat')
-wget.download('https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/others/install.vbs','C:\Temp\install.vbs')
+urllib.request.urlretrieve(
+    'https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/others/install.bat', 'C:\Temp\install.bat')
+urllib.request.urlretrieve(
+    'https://raw.githubusercontent.com/Edu4r0/SaludOcup-Web-Version/main/others/install.vbs', 'C:\Temp\install.vbs')
 
 subprocess.call(['cmd.exe', '/c', 'C:/Temp/install.bat'])
-# Especifica la ruta completa del archivo .vbs
-#ruta_vbs = r"C:\Temp\install.vbs"
+# ruta_vbs = r"C:\Temp\install.vbs"
 
 # Ejecuta el archivo .vbs con la función subprocess.call()
-#subprocess.call(['cscript.exe', ruta_vbs])
+# subprocess.call(['cscript.exe', ruta_vbs])
