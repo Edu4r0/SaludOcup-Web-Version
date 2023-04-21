29002f7ff880ef18473f961972092c94eb56920a
Edu4r0 <116533491+Edu4r0@users.noreply.github.com>
2023-04-21 14:12:45 -0500

    OK

--- image.py
+++ image.py
@@ -5,7 +5,7 @@ def donload_image():
         opener = urllib.request.build_opener()
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         urllib.request.install_opener(opener)
-        path = 'C:\Temp\SaludOcup-Web-Version\image\image.png'
+        path = 'C:\\Temp\\SaludOcup-Web-Version\\image\\image.png'
         urllib.request.urlretrieve(
             'https://drive.google.com/uc?id=160swttBUh5qnlsCxcjAG71QT-1mkmvVm&export=download', path)
         
